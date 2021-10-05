7e9af0c0c21e9b863918f79bc41b4eb601df397d
Atheesh Rathnaweera <rathnaweeraatheesh72@gmail.com>
2021-10-06 03:04:32 +0530

    remove unwanted code

--- src/main/java/com/atheesh/vehiclestore/controllers/LoginController.java
+++ src/main/java/com/atheesh/vehiclestore/controllers/LoginController.java
@@ -1,15 +1,9 @@
 package com.atheesh.vehiclestore.controllers;
 
 import com.atheesh.vehiclestore.models.LoginRequest;
-import com.sun.net.httpserver.Authenticator;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
@@ -20,17 +14,6 @@ public class LoginController {
         loginRequest.setPassword(null);
 
         model.addAttribute("loginRequest", loginRequest);
-        return "login";
-    }
-
-    @PostMapping("/login/process")
-    public String actionLogin(Model model, @ModelAttribute("loginRequest") LoginRequest loginRequest) {
-        System.out.println("login post called " + loginRequest.toString());
-//        Notification notification = new Notification();
-//
-//        notification.setMessage("Login failed.");
-//        notification.setType("error");
-//        model.addAttribute("notification", notification);
         return "login";
     }
 }
