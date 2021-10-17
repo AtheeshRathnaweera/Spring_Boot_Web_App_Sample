ace4108e43b018f6a0251624b2f402d354f30058
Atheesh Rathnaweera <rathnaweeraatheesh72@gmail.com>
2021-10-18 04:12:59 +0530

    redirect to home if already logged in

--- src/main/java/com/atheesh/vehiclestore/controllers/LoginController.java
+++ src/main/java/com/atheesh/vehiclestore/controllers/LoginController.java
@@ -1,6 +1,9 @@
 package com.atheesh.vehiclestore.controllers;
 
 import com.atheesh.vehiclestore.models.LoginRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +12,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
     @GetMapping("/login")
     public String showLogin(Model model) {
-        System.out.println("login called");
-        LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setPassword(null);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        model.addAttribute("loginRequest", loginRequest);
-        return "login";
+        if (auth.getPrincipal() instanceof UserDetails) {
+            System.out.println("already logged in !");
+            return "redirect:/home";
+        } else {
+            System.out.println("login called");
+            LoginRequest loginRequest = new LoginRequest();
+            loginRequest.setPassword(null);
+
+            model.addAttribute("loginRequest", loginRequest);
+            return "login";
+        }
     }
 }
